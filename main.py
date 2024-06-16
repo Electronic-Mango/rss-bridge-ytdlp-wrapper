42f21e2f283bca4d07bbbae6d3a41ab1dd8db4ca
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2024-06-16 14:27:20 +0200

    Extract download URL from API request

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 from os import getenv
 from pathlib import Path
-from urllib.parse import urlencode
 from uuid import uuid4
 
 from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse, PlainTextResponse
-from httpx import get
+from httpx import URL, get
 from lxml.etree import SubElement, fromstring, strip_elements, tostring
 from yt_dlp import YoutubeDL
 
 load_dotenv()
 RSS_BRIDGE_URL = getenv("RSS_BRIDGE_URL")
-DOWNLOAD_API_URL = getenv("DOWNLOAD_API_URL")
 DEFAULT_MEDIA_NAMESPACE = getenv("DEFAULT_MEDIA_NAMESPACE", "http://search.yahoo.com/mrss/")
 ENCODING = "UTF-8"
 VIDEO_FILENAME = str(uuid4())
@@ -25,19 +23,20 @@ def rss(request: Request, remove_existing_media: bool = False):
     params = dict(request.query_params) | {"format": "Mrss"}
     params.pop("remove_existing_media", None)
     bridge_response = get(RSS_BRIDGE_URL, params=params)
-    extended_response = insert_media(bridge_response.text.encode(ENCODING), remove_existing_media)
+    rss_text = bridge_response.text.encode(ENCODING)
+    extended_response = insert_media(rss_text, remove_existing_media, request.base_url)
     return PlainTextResponse(extended_response)
 
 
-def insert_media(xml: bytes, remove_existing_media: bool) -> str:
+def insert_media(xml: bytes, remove_existing_media: bool, base_url: URL) -> str:
+    download_url = base_url.replace(path="/download")
     tree = fromstring(xml)
     media_namespace = (tree.nsmap or {}).get("media", DEFAULT_MEDIA_NAMESPACE)
     if remove_existing_media:
         strip_elements(tree, f"{{{media_namespace}}}content")
     for item in tree.xpath("//item"):
-        media_url_query = urlencode({"video_url": item.find("link").text})
-        media_url = f"{DOWNLOAD_API_URL}/download?{media_url_query}"
-        SubElement(item, f"{{{media_namespace}}}content", {"url": media_url})
+        item_download_url = download_url.include_query_params(video_url=item.find("link").text)
+        SubElement(item, f"{{{media_namespace}}}content", {"url": str(item_download_url)})
     return tostring(tree, xml_declaration=True, pretty_print=True, encoding=ENCODING)
 
 
